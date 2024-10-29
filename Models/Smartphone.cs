9fde8fa1084c0284ca76c20e741ee072e3e25c2a
Adriane <adrianepipito@hotmail.com>
2024-10-29 12:32:31 -0300

    Desafio concluídogit add .

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,7 +3,6 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
         protected string Modelo { get; set; }
 
         protected string IMEI { get; set; }
@@ -16,7 +15,6 @@ namespace DesafioPOO.Models
             Modelo = modelo;
             IMEI = imei;
             Memoria = memoria;
-            // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
         public void Ligar()
